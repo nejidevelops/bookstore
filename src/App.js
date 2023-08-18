b4d2098449e628714db81bdf4d134956dcef5bfa
Newton Ombese <newtonombese1@gmail.com>
2023-08-18 15:40:56 +0300

    :recycle:Refactor index and app js files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Booklist from './components/Booklist';
+import AddBook from './components/AddBook';
+
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<AddBook />} />
+        <Route path="/categories" element={<Booklist />} />
+      </Routes>
     </div>
   );
 }
