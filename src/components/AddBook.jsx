39f1efcb5e8d117e9affde00d2ef6912085a4d89
Newton Ombese <newtonombese1@gmail.com>
2023-08-24 15:23:51 +0300

    Implement addition ofbooks

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/bookSlice';
 
 function AddBook() {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      dispatch(addBook({
+        title,
+        author,
+        category: 'Fiction',
+        item_id: Date.now().toString(),
+      }));
+      setTitle('');
+      setAuthor('');
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h4>Add New Book</h4>
       <input
         type="text"
         placeholder="Add Book Title ..."
         name="title"
         className="bookTitle"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <input
         type="text"
         placeholder="Add Author Name"
         name="author"
         className="authorName"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
       />
       <button type="submit" className="addBook">
         Add Book
