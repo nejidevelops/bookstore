87cbb017c81fb9a406c60afa94dd336fac2c1b63
Newton Ombese <newtonombese1@gmail.com>
2023-08-24 07:02:39 +0300

    Add bookSlice redux component

---
+++ src/redux/books/bookSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  numOfBooks: [],
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.numOfBooks.push(action.payload);
+    },
+
+    removeBook: (state, action) => {
+      state.numOfBooks = state.numOfBooks.filter((book) => book !== action.payload);
+    },
+  },
+});
+
+export default bookSlice.reducer;
+export const { addBook, removeBook } = bookSlice.actions;
