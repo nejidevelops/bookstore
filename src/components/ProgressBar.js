3b4a2a6defabfeaf1489e26f5a7a0d1f50f0bbdb
Newton Ombese <newtonombese1@gmail.com>
2023-08-25 16:20:24 +0300

    Add progressBar js file

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,6 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+const ProgressBar = () => <div role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" />;
+
+export default ProgressBar;
