ad4b8eca5b91c05d8fd5b88e01f076d5f790ac40
Newton Ombese <newtonombese1@gmail.com>
2023-08-25 16:19:47 +0300

    Add Bookitem component

---
+++ src/components/BookItem.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Book from './Book';
+import Loader from './Tracker';
+import { getBooksData } from '../redux/books/booksSlice';
+
+const BookLists = () => {
+  const {
+    books, isLoading, error, isSuccessful,
+  } = useSelector((store) => store.book);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooksData());
+  }, [dispatch, isSuccessful]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
+  if (error) return <h2>Something went wrong</h2>;
+  if (books.length === 0) return <h2>No books available. Please come check later</h2>;
+  return books.map((book) => (
+    <div key={book.id}>
+      <Book key={book.id} book={book} />
+    </div>
+  ));
+};
+
+export default BookLists;
